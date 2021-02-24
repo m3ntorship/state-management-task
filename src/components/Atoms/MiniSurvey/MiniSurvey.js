3bf76e5573a527ce3909a3ef3bfa365d3b19d6ea
hebasaadosman <hebaosman443@gmail.com>
2021-02-24 12:33:33 +0300

    dynamic span of ltteters instead of svg options

--- src/components/Atoms/MiniSurvey/MiniSurvey.js
+++ src/components/Atoms/MiniSurvey/MiniSurvey.js
@@ -1,16 +1,20 @@
 import React from "react";
 import imagePost from "../../../img/Frame 145.png";
+import OptionOfSurvey from "../../Atoms/OptionOfSurvey/OptionOfSurvey";
 function MiniSurvey(props) {
   const Options = [
     { id: "1", optionName: "front", optionsInpVals: ["react", "vue"] },
     {
       id: "2",
       optionName: "back",
-      optionsInpVals: [ "node", "nested" ],
+      optionsInpVals: ["node", "nested"],
     },
   ];
   const { image = imagePost, MiniSurveyOptions = Options } = props;
-
+  const letters = (() => {
+    const caps = [...Array(26)].map((val, i) => String.fromCharCode(i + 65));
+    return caps;
+  })();
   return (
     <div>
       <div className=" grid gap-x-2 gap-y-4 mb-m rounded-md relative">
@@ -28,31 +32,16 @@ function MiniSurvey(props) {
           <h3 className="text-sm font-normal text-dark-grey mb-xs w-full">
             {MiniSurveyOption.optionName}
           </h3>
-          {MiniSurveyOption.optionsInpVals.map((optionsInpVal) => (
-            <div
-              key={optionsInpVal}
-              className="bg-grey-bg hover:bg-grey-shd6 flex items-center w-37xl p-xs mb-xs rounded-md"
-            >
-              <svg
-                className="mr-s"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M0 4C0 1.79086 1.79086 0 4 0H20C22.2091 0 24 1.79086 24 4V20C24 22.2091 22.2091 24 20 24H4C1.79086 24 0 22.2091 0 20V4Z"
-                  fill="white"
-                />
-                <path
-                  d="M14.085 14.4023H9.91504L8.97852 17H7.625L11.4258 7.04688H12.5742L16.3818 17H15.0352L14.085 14.4023ZM10.3115 13.3223H13.6953L12 8.66699L10.3115 13.3223Z"
-                  fill="#5B6366"
-                />
-              </svg>
-              <h3 className="text-sm font-normal text-dark">{optionsInpVal}</h3>
-            </div>
-          ))}
+          {MiniSurveyOption.optionsInpVals.map((optionsInpVal, index) => {
+            const letter = letters[index];
+            return (
+              <OptionOfSurvey
+                alpha={letter}
+                index={index}
+                optionsInpVal={optionsInpVal}
+              />
+            );
+          })}
         </div>
       ))}
     </div>
