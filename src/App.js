3b744ec8abd1c85ad1dbde82c602d81f0c2e738e
hebasaadosman <hebaosman443@gmail.com>
2021-02-25 03:51:54 +0300

    test render components

--- src/App.js
+++ src/App.js
@@ -2,23 +2,40 @@ import React from "react";
 import PostEdit from "./components/Organisms/PostEdit/PostEdit";
 import Posts from "./features/posts/Posts.jsx";
 import ViewImagePoll from "./components/Organisms/ImagePoll/ImagePoll";
-import ViewImagePollResult from "./components/Organisms/ViewImagePollResult/ViewImagePollResult";
+// import ViewImagePollResult from "./components/Organisms/ViewImagePollResult/ViewImagePollResult";
 import ViewTextPoll from "./components/Organisms/ViewTextPoll/ViewTextPoll";
 import ViewMiniSurvey from "./components/Organisms/ViewMiniSurvey/ViewMiniSurvey";
-import ViewTextPollReaults from "./components/Organisms/ViewTextPollReaults/ViewTextPollReaults";
-import ViewMiniSurveyResults from "./components/Organisms/ViewMiniSurveyResults/ViewMiniSurveyResults";
-
+// import ViewTextPollReaults from "./components/Organisms/ViewTextPollReaults/ViewTextPollReaults";
+// import ViewMiniSurveyResults from "./components/Organisms/ViewMiniSurveyResults/ViewMiniSurveyResults";
+import { useSelector } from "react-redux";
 const App = () => {
+  const posts = useSelector((state) => state.picklyPosts);
+
   return (
     <>
       <PostEdit />
       <Posts />
-      <ViewImagePoll />
+      {posts.map((post, index) => {
+        switch (post.type) {
+          case "Image Poll":
+            <ViewImagePoll key={index + 100} />;
+            break;
+          case "Text Poll":
+            <ViewTextPoll key={index} />;
+            break;
+          case "Mini survey":
+            <ViewMiniSurvey key={index + 200} />;
+            break;
+          default:
+            return;
+        }
+      })}
+      {/* <ViewImagePoll />
       <ViewImagePollResult />
       <ViewTextPoll />
       <ViewMiniSurvey />
       <ViewTextPollReaults />
-      <ViewMiniSurveyResults />
+      <ViewMiniSurveyResults /> */}
     </>
   );
 };
